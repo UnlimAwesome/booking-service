d753c76dd397dbceb52482eee3026382db100d53
UnlimAwesome <mnauspb@yandex.ru>
2025-06-01 20:56:41 +0300

    feat: signup

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,10 +1,10 @@
-import { LoginForm } from '@features/Auth/LoginForm/ui/LoginForm';
+import { SignupForm } from '@features/Auth/SignupForm/ui/SignupForm';
 
 export default function Page() {
 	return (
 		<div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
 			<div className='w-full max-w-sm'>
-				<LoginForm />
+				<SignupForm />
 			</div>
 		</div>
 	);
