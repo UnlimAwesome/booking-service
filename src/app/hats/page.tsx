fb01c15ae7a0de86155b7e5b1c04a4dd0d28c679
UnlimAwesome <mnauspb@yandex.ru>
2025-06-05 00:16:41 +0300

    hat cards

--- src/app/hats/page.tsx
+++ src/app/hats/page.tsx
@@ -1,3 +1,4 @@
+import { HatCard } from '@/client/entities/Hat/Card/ui/Card';
 import { initSupabase } from '@/server/lib/supabase/init';
 
 export default async function Page() {
@@ -6,31 +7,14 @@ export default async function Page() {
 	console.log(await supabase.auth.getUser());
 	return (
 		<div className='flex size-full items-center justify-center'>
-			<div className='w-full max-w-sm'>
-				<ul>
-					<table className='w-full table-auto'>
-						<thead>
-							<tr>
-								{data?.[0] &&
-									Object.keys(data[0]).map((key) => (
-										<th key={key}>{key}</th>
-									))}
-							</tr>
-						</thead>
-						<tbody>
-							{data?.map((hat) => (
-								<tr key={hat.id}>
-									{Object.values(hat).map((value) => (
-										<td key={String(value)}>
-											{String(value)}
-										</td>
-									))}
-								</tr>
-							))}
-						</tbody>
-					</table>
-					{error?.message}
-				</ul>
+			<div className='w-full flex gap-8'>
+				{data?.map((hat) => (
+					<HatCard
+						key={hat.id}
+						hat={hat}
+					/>
+				))}
+				{error && <p>{error.message}</p>}
 			</div>
 		</div>
 	);
