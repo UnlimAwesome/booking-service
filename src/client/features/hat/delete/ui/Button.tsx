44957e37ffd7f7dba47ad52c1ac3c83c46d83db1
UnlimAwesome <mnauspb@yandex.ru>
2025-06-07 01:36:51 +0300

    feat: delete button spin loading animation

--- src/client/features/hat/delete/ui/Button.tsx
+++ src/client/features/hat/delete/ui/Button.tsx
@@ -1,25 +1,37 @@
 'use client';
 import { Button as ButtonPrimitive } from '@components/ui/button';
-import { Trash } from 'lucide-react';
+import { LoaderCircle, Trash } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { deleteHat } from '../api/delete';
 import { HasPermission } from '@components/HasPermission/HasPermission';
 
 export const Button = memo(function Button(props: { hatId: number }) {
 	const { hatId } = props;
+	const [loading, setLoading] = useState(false);
 	const router = useRouter();
 	return (
 		<HasPermission permission={'hat.deleteOwn'}>
 			<ButtonPrimitive
+				disabled={loading}
 				variant={'destructive'}
 				size={'icon'}
 				onClick={async () => {
+					// eslint-disable-next-line @typescript-eslint/no-unused-vars
+					setLoading((prev) => true);
 					const { error } = await deleteHat(hatId);
-					if (!error) router.refresh();
+					if (!error) {
+						router.refresh();
+						// eslint-disable-next-line @typescript-eslint/no-unused-vars
+						setLoading((prev) => false);
+					}
 				}}
 			>
-				<Trash />
+				{loading ? (
+					<LoaderCircle className='animate-spin' />
+				) : (
+					<Trash />
+				)}
 			</ButtonPrimitive>
 		</HasPermission>
 	);
